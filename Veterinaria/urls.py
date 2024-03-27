49097979c70fc210433b5b90ddde5e46cf711a2a
angel_teyul <willson.alvarado@outlook.com>
2024-03-26 23:57:27 -0600

    endpoints updated, dockerfile and docker-compose files added

--- Veterinaria/urls.py
+++ Veterinaria/urls.py
@@ -5,15 +5,16 @@ from rest_framework import routers
 from Veterinaria import views
 
 router = routers.DefaultRouter()
-router.register(r'dueno', views.DuenoView, 'dueno')
-router.register(r'especie', views.EspecieView, 'especie')
-router.register(r'genero', views.GeneroView, 'genero')
-router.register(r'raza', views.RazaView, 'raza')
-router.register(r'mascota', views.MascotaView, 'mascota')
-router.register(r'desparasitante', views.DesparasitanteView, 'desparasitante')
-router.register(r'fichadesparacitacion', views.FichaDesparacitacionView, 'fichadesparacitacion')
-router.register(r'estado', views.EstadoView, 'estado')
-router.register(r'cita', views.CitaView, 'cita')
+
+router.register(r'duenos', views.DuenoView, 'dueno')
+router.register(r'especies', views.EspecieView, 'especie')
+router.register(r'generos', views.GeneroView, 'genero')
+router.register(r'razas', views.RazaView, 'raza')
+router.register(r'mascotas', views.MascotaView, 'mascota')
+router.register(r'desparasitantes', views.DesparasitanteView, 'desparasitante')
+router.register(r'desparacitaciones', views.FichaDesparacitacionView, 'fichadesparacitacion')
+router.register(r'estados', views.EstadoView, 'estado')
+router.register(r'citas', views.CitaView, 'cita')
 
 urlpatterns = [
     path('api/', include(router.urls)),
